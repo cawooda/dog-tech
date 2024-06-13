55eaa38f8bfa7dd99a2eaf727c8e61300fd3bbdc
Andrew Cawood <cawooda@gmail.com>
2024-06-13 12:47:21 +1000

    rnd

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,21 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = process.env.DB_URL
-	? new Sequelize(process.env.DB_URL)
+	? new Sequelize(process.env.DB_URL, {
+			dialect: 'postgres',
+			dialectOptions: {
+				ssl: {
+					require: true,
+					rejectUnauthorized: false,
+				},
+			},
+		})
 	: new Sequelize(
 			process.env.DB_NAME,
 			process.env.DB_USER,
 			process.env.DB_PASSWORD,
 			{
-				host: 'localhost',
+				host: process.env.DB_HOST || 'localhost',
 				dialect: 'postgres',
 				dialectOptions: {
 					ssl: {
